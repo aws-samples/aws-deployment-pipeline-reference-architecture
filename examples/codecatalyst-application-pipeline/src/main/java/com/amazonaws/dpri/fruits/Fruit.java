2477b039fec5dd548b24970da0dee544495d1454
Casey Lee <caseypl@amazon.com>
2023-01-06 13:08:57 -0800

    Merge from upstream commit 4141c34f55d9e63dbdd876ef332e4ca997c96a94

--- examples/codecatalyst-application-pipeline/src/main/java/com/amazonaws/dpri/fruits/Fruit.java
+++ examples/codecatalyst-application-pipeline/src/main/java/com/amazonaws/dpri/fruits/Fruit.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -14,7 +15,7 @@ public class Fruit {
   /**
    * Unique ID for this fruit.
    */
-  private @Id @GeneratedValue Long id;
+  private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
 
   /**
    * Name for this fruit.
