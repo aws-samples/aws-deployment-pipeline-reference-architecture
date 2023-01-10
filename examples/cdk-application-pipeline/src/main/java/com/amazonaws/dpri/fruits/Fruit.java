412c96f6240556e86ea065e6a8fb32382b4b0388
Casey Lee <caseypl@amazon.com>
2023-01-10 10:12:35 -0800

    Merge from upstream commit c42c327f785355f49708c3ac186bb69bc33dc1f3

--- examples/cdk-application-pipeline/src/main/java/com/amazonaws/dpri/fruits/Fruit.java
+++ examples/cdk-application-pipeline/src/main/java/com/amazonaws/dpri/fruits/Fruit.java
@@ -2,10 +2,10 @@ package com.amazonaws.dpri.fruits;
 
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 /**
  * Entity for persisting fruits.
